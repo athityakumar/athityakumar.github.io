5adf3057af0a48e6d698b25a76ac500ebbcec25d
athityakumar <athityakumar@gmail.com>
2016-12-22 23:54:32 +0530

    Restructured repo - part 1

---
+++ index.rb
@@ -0,0 +1,17 @@
+# Files associated -
+
+# Input
+# -----
+# A -> data/metadata.json
+# B -> data/config.json
+# C -> data/posts/*.json
+
+# Output
+# ------
+# index.html from 
+# blog/index.html   
+# blog/tag/*/index.html
+# blog/post/*/index.html
+# blog/page*/index.html
+# javascript/home.js for typed_string
+# javascript/search.js for local search updation
