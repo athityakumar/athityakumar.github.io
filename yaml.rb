a00f0c496dfb38eb0e5401638d6eae13c528a457
Athitya <athityakumar@gmail.com>
2017-06-11 22:28:44 +0530

    YAML loading works

--- yaml.rb
+++ yaml.rb
@@ -4,7 +4,7 @@ yaml = YAML.load(File.read("in.yaml"))
 md = yaml.delete "html"
 puts yaml
 puts "\n\n"
-puts Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(md)
+puts Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(no_intra_emphasis: true, tables: true, autolink: true, strikethrough: true, with_toc_data: true)).render(md)
 
 str = "--- Hey --- # --- Hello ---"
 pattern = /---.*?---/
